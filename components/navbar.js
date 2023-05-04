3a9b7d667068e163deb2f9e47fd072586860e4a0
Nícholas Eras <48137930+Nicholasmais@users.noreply.github.com>
2023-05-04 17:18:44 -0300

    tentativa falha de menu hambuhuer em componente

--- components/navbar.js
+++ components/navbar.js
@@ -1,36 +1,13 @@
 class NavBar extends HTMLElement {
   constructor() {
     super();
+    this.attachShadow({mode: 'open'});
+    this.isOpen = false;
   }
   connectedCallback() {
-    this.innerHTML = `
-      <style>
-        header{
-          display: flex;
-          justify-content: center;
-        }              
-        nav{
-          display: inline-flex;
-          flex-direction: row;
-          background-color: rgb(255, 255, 196);
-          gap: 2rem;
-          padding: .5rem;
-          margin-bottom: 2rem;
-          border-radius: 5px 10px 15px 20px;
-        }
-        a{
-          color:black;
-          text-decoration: none;
-        }
-        a:hover{
-          cursor: pointer;
-          transform: scale(1.25);
-        }
-        nav a.active{
-          text-decoration: underline;
-        }
-      </style>
-      <header>
+    this.shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="components\\styles.css">
+      <header class="header">
         <nav>
           <a class="all-competences" href="/index.html">
             Ver competências
@@ -47,6 +24,12 @@ class NavBar extends HTMLElement {
         </nav>
       </header>
     `;
+    const header = this.shadowRoot.querySelector('.header');
+    header.addEventListener('click', () => {
+      header.classList.toggle('clicked');
+      this.isOpen = !this.isOpen; // alterna o estado da variável entre true e false
+      document.getElementsByTagName('a').style.visibility = this.isOpen ? 'visible' : 'hidden'; // altera a visibilidade com base no estado atual da variável
+    });    
   }
 }
 
